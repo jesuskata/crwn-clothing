2829bb274661e0be015832666b68f4d895e8fe76
jesuskata <rzarate.alex@gmail.com>
2020-02-04 11:48:45 -0600

    refactor: 💡 modify the js to jsx index file
    Change the js index file to an jsx react file

---
+++ src/index.jsx
@@ -0,0 +1,17 @@
+// Dependencies
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+// Components
+import App from './App';
+
+// Styles
+import './index.css';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
