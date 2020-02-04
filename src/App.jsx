cbed445f0c31aee42ee75d1515d2820911222a16
jesuskata <rzarate.alex@gmail.com>
2020-02-04 11:50:04 -0600

    feat: 🎸 add routing to the project
    Add the switch and route components to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 // Dependencies
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+// Styles
 import './App.css';
 
 // Components
 import { HomePage } from './pages/HomePage';
 
-function App() {
-  return (
-    <div>
-      <HomePage />
-    </div>
-  );
-}
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
+const App = () => (
+  <div>
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/hats" component={HatsPage} />
+    </Switch>
+  </div>
+);
 
 export default App;
