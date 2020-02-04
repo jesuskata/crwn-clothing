780168d0661f0337fc5bff0d4f5ffcd5707710c0
jesuskata <rzarate.alex@gmail.com>
2020-02-04 02:37:45 -0600

    refactor: 💡 move to the new structure the initial files
    The HomePage component and styles was moved to the new structure

---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,14 @@
+// Dependencies
+import React from 'react';
+
+// Components
+import { Directory } from '../../components/Directory';
+
+// Styles
+import './HomePage.scss';
+
+export const HomePage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
