5700811f435ea462b4ceb203a81159a7838293fb
jesuskata <rzarate.alex@gmail.com>
2020-02-06 16:55:28 -0600

    feat: 🎸 create the new sign in and sign up page

---
+++ src/pages/SigninAndSignup/index.jsx
@@ -0,0 +1,14 @@
+// Dependencies
+import React from 'react';
+
+// Components
+import { SignIn } from '../../components/SignIn';
+
+// Styles
+import './styles.scss';
+
+export const SigninAndSignup = () => (
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+  </div>
+);
