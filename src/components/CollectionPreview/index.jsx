d1ecc0790b5ad0441000db76520685a33bf62140
jesuskata <rzarate.alex@gmail.com>
2020-02-05 20:53:37 -0600

    feat: 🎸 create the collection preview component

---
+++ src/components/CollectionPreview/index.jsx
@@ -0,0 +1,26 @@
+// Dependencies
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// Styles
+import './styles.scss';
+
+export const CollectionPreview = ({ title, items }) => (
+  <div className="collection-preview">
+    <h1 className="title">{title.toUpperCase()}</h1>
+    <div className="preview">
+      {
+        items
+          .filter((item, idx) => idx < 4)
+          .map(item => (
+            <div key={item.id}>{item.name}</div>
+          ))
+      }
+    </div>
+  </div>
+);
+
+CollectionPreview.propTypes = {
+  title: PropTypes.string,
+  items: PropTypes.arrayOf(PropTypes.any)
+};
