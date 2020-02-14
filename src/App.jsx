d719b9cd6edd9b3fbd09d584b35aee520c20fdbf
jesuskata <rzarate.alex@gmail.com>
2020-02-14 01:37:35 -0600

    refactor: 💡 refactor and clean code

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,6 @@ class App extends React.Component {
               ...snapShot.data()
             }
           });
-          console.log('state: ', this.state); // eslint-disable-line
         });
       }
       this.setState({ currentUser: userAuth });
