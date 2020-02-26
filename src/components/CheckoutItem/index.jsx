35ea73297df19d64c6d46a70cc3fb8d36e2898fb
jesuskata <rzarate.alex@gmail.com>
2020-02-26 02:38:02 -0600

    refactor: 💡 add the arrows and the functionalities

--- src/components/CheckoutItem/index.jsx
+++ src/components/CheckoutItem/index.jsx
@@ -6,12 +6,18 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 // Redux
-import { clearItemFromCart } from '../../store/actions/cartActions';
+import {
+  clearItemFromCart,
+  addItem as addItemAction,
+  removeItem as removeItemAction
+} from '../../store/actions/cartActions';
 
 // Styles
 import './styles.scss';
 
-const CheckoutItemFn = ({ cartItem, clearItem }) => {
+const CheckoutItemFn = ({
+  cartItem, clearItem, addItem, removeItem
+}) => {
   const {
     name, imageUrl, price, quantity
   } = cartItem;
@@ -22,7 +28,11 @@ const CheckoutItemFn = ({ cartItem, clearItem }) => {
         <img src={imageUrl} alt="item" />
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={() => removeItem(cartItem)}>&#10094;</div>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={() => addItem(cartItem)}>&#10095;</div>
+      </span>
       <span className="price">{price}</span>
       <span className="remove-button" onClick={() => clearItem(cartItem)}>&#10005;</span>
     </div>
@@ -31,12 +41,16 @@ const CheckoutItemFn = ({ cartItem, clearItem }) => {
 
 CheckoutItemFn.propTypes = {
   cartItem: PropTypes.objectOf(PropTypes.any),
-  clearItem: PropTypes.func
+  clearItem: PropTypes.func,
+  addItem: PropTypes.func,
+  removeItem: PropTypes.func,
 };
 
 // This is another way to do the dispatch
 const mapDispatchToProps = dispatch => ({
-  clearItem: item => dispatch(clearItemFromCart(item))
+  clearItem: item => dispatch(clearItemFromCart(item)),
+  addItem: item => dispatch(addItemAction(item)),
+  removeItem: item => dispatch(removeItemAction(item))
 });
 
 export const CheckoutItem = connect(null, mapDispatchToProps)(CheckoutItemFn);
