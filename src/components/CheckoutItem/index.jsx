6289905d40f153b960ddcef38fcb4ffe146bad0e
jesuskata <rzarate.alex@gmail.com>
2020-02-26 01:53:53 -0600

    feat: 🎸 create a new checkout item component

---
+++ src/components/CheckoutItem/index.jsx
@@ -0,0 +1,26 @@
+// Dependencies
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// Styles
+import './styles.scss';
+
+export const CheckoutItem = ({
+  cartItem: {
+    name, imageUrl, price, quantity
+  }
+}) => (
+  <div className="checkout-item">
+    <div className="image-container">
+      <img src={imageUrl} alt="item" />
+    </div>
+    <span className="name">{name}</span>
+    <span className="quantity">{quantity}</span>
+    <span className="price">{price}</span>
+    <span className="remove-button">&#10005;</span>
+  </div>
+);
+
+CheckoutItem.propTypes = {
+  cartItem: PropTypes.arrayOf(PropTypes.any)
+};
