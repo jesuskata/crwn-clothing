62cec8063ae184c3fc0384e9689e7f39dcd423d0
jesuskata <rzarate.alex@gmail.com>
2020-02-26 02:17:10 -0600

    feat: 🎸 create the redux action to remove items from the cart

--- src/components/CheckoutItem/index.jsx
+++ src/components/CheckoutItem/index.jsx
@@ -1,26 +1,42 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 // Dependencies
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+// Redux
+import { clearItemFromCart } from '../../store/actions/cartActions';
 
 // Styles
 import './styles.scss';
 
-export const CheckoutItem = ({
-  cartItem: {
+const CheckoutItemFn = ({ cartItem, clearItem }) => {
+  const {
     name, imageUrl, price, quantity
-  }
-}) => (
-  <div className="checkout-item">
-    <div className="image-container">
-      <img src={imageUrl} alt="item" />
-    </div>
-    <span className="name">{name}</span>
-    <span className="quantity">{quantity}</span>
-    <span className="price">{price}</span>
-    <span className="remove-button">&#10005;</span>
-  </div>
-);
+  } = cartItem;
 
-CheckoutItem.propTypes = {
-  cartItem: PropTypes.arrayOf(PropTypes.any)
+  return (
+    <div className="checkout-item">
+      <div className="image-container">
+        <img src={imageUrl} alt="item" />
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">{quantity}</span>
+      <span className="price">{price}</span>
+      <span className="remove-button" onClick={() => clearItem(cartItem)}>&#10005;</span>
+    </div>
+  );
 };
+
+CheckoutItemFn.propTypes = {
+  cartItem: PropTypes.objectOf(PropTypes.any),
+  clearItem: PropTypes.func
+};
+
+// This is another way to do the dispatch
+const mapDispatchToProps = dispatch => ({
+  clearItem: item => dispatch(clearItemFromCart(item))
+});
+
+export const CheckoutItem = connect(null, mapDispatchToProps)(CheckoutItemFn);
--- src/store/actionTypes/index.js
+++ src/store/actionTypes/index.js
@@ -4,5 +4,6 @@ export const userActionTypes = {
 
 export const cartActionTypes = {
   TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
-  ADD_ITEM: 'ADD_ITEM'
+  ADD_ITEM: 'ADD_ITEM',
+  CLEAR_ITEM_FROM_CART: 'CLEAR_ITEM_FROM_CART'
 };
