a087c1eb68fa26f9680f824272b184197cc02db9
jesuskata <rzarate.alex@gmail.com>
2020-02-27 00:25:39 -0600

    feat: 🎸 integrate the directory reducer into the rootReducer

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -6,18 +6,20 @@ import storage from 'redux-persist/lib/storage';
 // Reducers
 import { userReducer } from './reducers/userReducer';
 import { cartReducer } from './reducers/cartReducer';
+import { directoryReducer } from './reducers/directoryReducer';
 
 const persistConfig = {
   key: 'root',
   storage,
   whitelist: [
-    'cart'
+    'cart', 'directory'
   ]
 };
 
 const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  directory: directoryReducer
 });
 
 export default persistReducer(persistConfig, rootReducer);
