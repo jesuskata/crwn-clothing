8ea39e11fdc007cb544d0432d8d11e6140080071
jesuskata <rzarate.alex@gmail.com>
2020-03-02 13:44:36 -0600

    refactor: 💡 modify the selector according with the new object

--- src/store/selectors/shop/index.js
+++ src/store/selectors/shop/index.js
@@ -1,14 +1,6 @@
 // Dependencies
 import { createSelector } from 'reselect';
 
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5
-};
-
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
@@ -19,8 +11,6 @@ export const selectCollections = createSelector(
 export const selectCollection = collectionUrlParam => (
   createSelector(
     [selectCollections],
-    collections => collections.find(
-      collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-    )
+    collections => collections[collectionUrlParam]
   )
 );
