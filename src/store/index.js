f28feec60a8591878ee491a961cf6d57e9cd744c
jesuskata <rzarate.alex@gmail.com>
2020-10-31 01:18:22 -0600

    feat: 🎸 delete logger from production

--- src/store/index.js
+++ src/store/index.js
@@ -6,9 +6,11 @@ import logger from 'redux-logger';
 // Reducers
 import rootReducer from './rootReducer';
 
-const middlewares = [
-  logger
-];
+const middlewares = [];
+
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
 
 const store = createStore(
   rootReducer,
