f1c59f82ac382c414c10202ebde85861bf08d1a4
jesuskata <rzarate.alex@gmail.com>
2021-01-04 21:24:13 -0600

    style: 💄 make the application mobile friendly

--- client/src/pages/HomePage/styles/index.jsx
+++ client/src/pages/HomePage/styles/index.jsx
@@ -6,4 +6,8 @@ export const HomePageContainer = styled.div`
   flex-direction: column;
   align-items: center;
   padding: 20px 80px;
+
+  @media screen and (max-width: 800px) {
+    padding: unset;
+  }
 `;
