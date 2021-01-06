a6a5cc0bf3e648ec3ec6411c57d9103ea4b9485c
jesuskata <rzarate.alex@gmail.com>
2021-01-06 12:22:03 -0600

    fix: 🐛 copy the server.js file from yihua

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
-const compression = require('compression');
-const enforce = require('express-sslify');
+var enforce = require('express-sslify');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
@@ -13,19 +12,17 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(cors());
 
-app.get('/service-worker.js', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
-})
-
 if (process.env.NODE_ENV === 'production') {
-  app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('/service-worker.js', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+  });
 
   app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
@@ -34,8 +31,8 @@ if (process.env.NODE_ENV === 'production') {
 
 app.listen(port, error => {
   if (error) throw error;
-  console.log(`Server running on PORT ${port}`);
-})
+  console.log('Server running on port ' + port);
+});
 
 app.post('/payment', (req, res) => {
   const body = {
@@ -48,7 +45,7 @@ app.post('/payment', (req, res) => {
     if (stripeErr) {
       res.status(500).send({ error: stripeErr });
     } else {
-      res.status(200).send({ success: stripeErr });
+      res.status(200).send({ success: stripeRes });
     }
   });
-})
+});
